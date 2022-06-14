588350d6855fbce1d26b32794f7da9cec611c526
Jack Glass <jackadrianglass@gmail.com>
2022-06-13 21:07:27 -0600

    [photo_groove] Finished ch 2 of Elm in Action

--- photo_groove/src/PhotoGroove.elm
+++ photo_groove/src/PhotoGroove.elm
@@ -1,7 +1,9 @@
 module PhotoGroove exposing (main)
 
+import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
 import List
 
 initialModel =
@@ -11,6 +13,7 @@ initialModel =
     , { url = "3.jpeg" }
     ]
   , selectedUrl = "1.jpeg"
+  , message = ""
   }
 
 urlPrefix = "http://elm-in-action.com/"
@@ -20,21 +23,41 @@ view model =
   [ h1 [] [ text "Photo Groove" ]
   , div [id "thumbnails" ]
     ( List.map
-      (\photo -> viewThumbnail model.selectedUrl photo)
+      (viewThumbnail model.selectedUrl) -- Partial application of a function
       model.photos
     )
+  , img
+    [ class "large"
+    , src (urlPrefix ++ "large/" ++ model.selectedUrl)
+    ]
+    []
+  , div
+    []
+    [ text model.message ]
   ]
 
 viewThumbnail selectedUrl thumb =
-  if selectedUrl == thumb.url then
-    img
-      [ src (urlPrefix ++ thumb.url)
-      , class "selected"
-      ]
-      []
-  else
-    img
-      [ src (urlPrefix ++ thumb.url) ]
-      []
+  img
+  [ src (urlPrefix ++ thumb.url)
+  {- describes a list of class attributs and the predicate on which it depends -}
+  , classList [ ( "selected", selectedUrl == thumb.url ) ]
+  {-
+  describes what events to send when a certain html event occurs
+  which in this case is on click
+  -}
+  , onClick { description = "ClickedPhoto", data = thumb.url }
+  ]
+  []
 
-main = view initialModel
+update msg model =
+  if msg.description == "ClickedPhoto" then
+    { model | selectedUrl = msg.data }
+  else
+    model
+
+main =
+  Browser.sandbox
+    { init = initialModel
+    , view = view
+    , update = update
+    }

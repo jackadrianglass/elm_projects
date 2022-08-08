861283d0f60308739c72ea8ae80b666e051f3535
glassjack <jackadrianglass@gmail.com>
2022-08-07 20:53:16 -0600

    [cv] Playing with tailwind css for CV

--- cv_website/src/Main.elm
+++ cv_website/src/Main.elm
@@ -1,41 +1,40 @@
-module Main exposing (main)
-import Browser
-import Html exposing (Html, button, div, text, ul)
-import Html.Events exposing (onClick)
+module Main exposing (..)
 
-zen_of_python =
-  """
-  Beautiful is better than ugly.
-  Explicit is better than implicit.
-  Simple is better than complex.
-  Complex is better than complicated.
-  Flat is better than nested.
-  Sparse is better than dense.
-  Readability counts.
-  Special cases aren't special enough to break the rules.
-  Although practicality beats purity.
-  Errors should never pass silently.
-  Unless explicitly silenced.
-  In the face of ambiguity, refuse the temptation to guess.
-  There should be one-- and preferably only one --obvious way to do it.
-  Although that way may not be obvious at first unless you're Dutch.
-  Now is better than never.
-  Although never is often better than *right* now.
-  If the implementation is hard to explain, it's a bad idea.
-  If the implementation is easy to explain, it may be a good idea.
-  Namespaces are one honking great idea -- let's do more of those!
-  """
+import Css
+import Css.Global
+import Html.Styled as Html
+import Html.Styled.Attributes as Attr
+import Tailwind.Breakpoints as Breakpoints
+import Tailwind.Utilities as Tw
+
+
+ferris =
+  Html.img
+      [ Attr.src "../assets/cuddlyferris.png"
+      , Attr.css
+        [ Tw.h_auto
+        , Tw.w_full
+        , Tw.bg_white
+        , Tw.rounded_full
+        , Tw.border_black
+        , Tw.border_4
+        ]
+      ] [ Html.text "Photo not found" ]
 
 main =
-  Browser.sandbox { init = 0, update = update, view = view }
-
-update msg model =
-  model
-
-view model =
-  div []
-    [ div [] [ text zen_of_python ]
+  Html.toUnstyled <|
+    Html.div
+    [ Attr.css
+      [ Tw.bg_blue_600
+      , Tw.rounded_lg
+      ]
     ]
+    [ Html.div []
+      [ ferris
+      ]
+    ]
+    
+
 
 {-
 The Plan:
